c4d199c1a0e153ae731de9710135417fd2480a5f
anhnt2989 <49859843+anhnt2989@users.noreply.github.com>
2019-06-05 11:54:48 +0700

    Update Java_MaxInArr.java

--- src/main/java/Java_MaxInArr.java
+++ src/main/java/Java_MaxInArr.java
@@ -11,7 +11,7 @@ public class Java_MaxInArr {
             if (size > 20) {
                 System.out.println("Size should not exceed 20");
             }
-        } while (size < 20);
+        } while (size > 20);
         array = new int[size];
         int i = 0;
         while (i < array.length) {
